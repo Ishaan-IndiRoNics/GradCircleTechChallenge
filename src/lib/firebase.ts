e4d69be01ee03c44c96c027103b10e440ab7012d
ishaan31211 <ishaan31211@gmail.com>
2026-01-16 18:16:44 +0000

    yr

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,7 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import { firebaseConfig } from './firebase-config';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDp_xFLo8-uABbPe8N37Xo-fqKPyJCp7E8",
+    authDomain: "petconnect-29149.firebaseapp.com",
+    projectId: "petconnect-29149",
+    storageBucket: "petconnect-29149.firebasestorage.app",
+    messagingSenderId: "165297069841",
+    appId: "1:165297069841:web:9f74bb705df813a6c9b6b0",
+  };
 
 const app = initializeApp(firebaseConfig);
 
